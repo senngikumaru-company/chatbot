6ece99983443cbffce661dab2e351cd4d5ed5a06
Sotaro Yagi <54488278+senngikumaru-company@users.noreply.github.com>
2024-09-30 05:14:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import openai
 import secret_keys  # 外部ファイルにAPI keyを保存
 
-openai.api_key = secret_keys.openai_api_key
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
 system_prompt = """
 あなたは優秀な料理研究家です。
